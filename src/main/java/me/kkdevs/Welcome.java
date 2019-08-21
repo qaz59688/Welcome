39c428b7b177e0fc2b0612c08c5519419250e366
Sergey Omelchenko <45960833+KKDevelop@users.noreply.github.com>
2019-08-21 17:35:20 +1000

    Added support for different versions

--- src/main/java/me/kkdevs/Welcome.java
+++ src/main/java/me/kkdevs/Welcome.java
@@ -3,9 +3,11 @@ package me.kkdevs;
 import cn.nukkit.Player;
 import cn.nukkit.event.EventHandler;
 import cn.nukkit.event.Listener;
+import cn.nukkit.event.player.PlayerJoinEvent;
 import cn.nukkit.event.server.DataPacketReceiveEvent;
 import cn.nukkit.network.protocol.SetLocalPlayerAsInitializedPacket;
 import cn.nukkit.plugin.PluginBase;
+import cn.nukkit.scheduler.NukkitRunnable;
 import cn.nukkit.utils.Config;
 
 import java.util.HashMap;
@@ -24,12 +26,17 @@ public class Welcome extends PluginBase implements Listener {
     public void onEnable() {
         this.getServer().getPluginManager().registerEvents(this, this);
         this.cfg = new Config("plugins/Welcome/config.yml", Config.YAML);
-        if (this.cfg.get("Title") == null || this.cfg.get("SubTitle") == null || this.cfg.get("BossBar_Text") == null) {
+        if (this.cfg.get("Title") == null || this.cfg.get("SubTitle") == null || this.cfg.get("BossBar_Text") == null || this.cfg.get("interval") == null) {
+            this.getLogger().alert("Some elements of the config or the config itself are missing, a new one is created!");
             this.cfg.set("Title", "§l§aName§fServer");
             this.cfg.set("SubTitle", "§l§b>> §fHave a nice game §b<<");
             this.cfg.set("BossBar_Text", "      §l§f>>  {rnd} Welcоme to {Title}{rnd}, §b{name}{rnd} | Online: {online}   §f<<");
             this.cfg.set("interval", 20);
             this.cfg.save(true);
+        } else if(this.cfg.get("protocol") == null) {
+            this.cfg.set("protocol", 361);
+            this.cfg.save(true);
+            getLogger().alert("There is no protocol in the config! Default 361");
         }
 
         this.Title = this.cfg.getString("Title");
@@ -49,10 +56,27 @@ public class Welcome extends PluginBase implements Listener {
     @EventHandler
     public void onPacketReceive(DataPacketReceiveEvent e) {
         Player p = e.getPlayer();
-        if(e.getPacket() instanceof SetLocalPlayerAsInitializedPacket) {
-            bossbar.put(p.getName().toLowerCase(), p.createBossBar(this.getText(p), 100));
-            p.sendTitle(Title, SubTitle);
-        }
+            if (this.cfg.getInt("protocol") >= 274) {
+                if (e.getPacket() instanceof SetLocalPlayerAsInitializedPacket) {
+                    bossbar.put(p.getName().toLowerCase(), p.createBossBar(this.getText(p), 100));
+                    p.sendTitle(Title, SubTitle);
+                }
+            }
+    }
+
+    @Deprecated
+    @EventHandler
+    public void onJoin(PlayerJoinEvent e) {
+        Player p = e.getPlayer();
+            if (this.cfg.getInt("protocol") < 274) {
+                bossbar.put(p.getName().toLowerCase(), p.createBossBar(this.getText(p), 100));
+                new NukkitRunnable() {
+                    public void run() {
+                        p.sendTitle(Title);
+                        p.setSubtitle(SubTitle);
+                    }
+                }.runTaskLater(this, 45);
+            }
     }
 
     public String getRndColor() {
